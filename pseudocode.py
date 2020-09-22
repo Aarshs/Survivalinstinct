10b2c2f6142a3ebe999e5d125e444699a840cf29
Aarsh Shah <aarsh.shah@rbe.sk.ca>
2020-09-22 03:48:51 +0000

    Added comments to pseudocode.py

--- pseudocode.py
+++ pseudocode.py
@@ -1,5 +1,22 @@
-# There are 2 different inventories including healables and tools
-# The healables will be: Knife, Flaregun, Metal
-# The tools will be: Bandages, Medkit, Painkiller
+# The game starts off with the player crash landing in a plane
 
+# He's the only survivor and is given the option to explore the jungle
+
+# There are 2 different inventories including healables and tools
+# The tools will be: Knife, Flaregun, Metal, Sword
+# The healables will be: Bandages, Medkit, Painkiller, Adrenaline Shot
 # The character will be able to move forward, back, right and left
+# He will also be able to find items as he explores
+
+
+# Depending on where he goes he will find different things
+# If player goes forward he will find another Medkit
+# If player goes right then he will encounter a wild animal
+# If player goes back he will be attacked by the villian
+# If player goes left he will find an adrenaline shot
+
+# If player is attacked then they will be given some options
+# Options include: Run, Attack, Defend, Heal
+
+# The goal of the game is to defeat the villian and get the flaregun back
+# The villian can sneak up on you at anytime and deal damage
