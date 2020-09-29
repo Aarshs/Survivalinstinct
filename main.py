a1a058268099998353de74199938f1838db2bc5e
Aarsh Shah <aarsh.shah@rbe.sk.ca>
2020-09-29 16:31:00 +0000

    added another nested dictionary to the inventory dictionary and added print statements accordingly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Course: CS 30
 # Period: 1
 # Date created: 2020-09-28
-# Date last modified: 2020-09-28
+# Date last modified: 2020-09-29
 # Name: Aarsh Shah
 # Description: Creates Nested Dictionary for
 # characters, inventories and locations
@@ -9,34 +9,64 @@
 # Creates a dictionary that stores information about characters
 characters = {
     "Immortal Man": {
-        "Description": "a horrifying beast whose goal is to hunt",
-        "Health": "500 health"},
+        "Description": "is a horrifying beast whose goal is to hunt",
+        "Age": "is 235 years old",
+        "Health": "has 500 health"},
     "James Dave": {
-        "Description": "the sole survivor in the plane crash",
-        "Health": "250 health"}
+        "Description": "is the sole survivor in the plane crash",
+        "Age": "is 26 years old",
+        "Health": "has 250 health"}
 }
 
 # A for loop that access's the characters dictionary and prints a description.
 for character, about in characters.items():
     for k, v in about.items():
-        print(f"{character} has {v}.")
-    print()
+        print(f"{character} {v}.")
 
-# Creates a dictionary that stores info about the items in each inventory.
+
+# Creates a dictionary that stores information about inventories
 inventories = {
     "Immortal Man": {
-        "Chainsaw": "125 Damage per hit weapon",
-        "Axe": "50 Damage per hit weapon",
-        "Flare Ammo": "ammo that James need to escape"},
+        "Chainsaw": {
+            "Description": "Large Weapon",
+            "Damage": 125,
+            "Uses": 8},
+        "Axe": {
+            "Description": "Medium Size Weapon",
+            "Damage": 50,
+            "Uses": 12},
+        "Flare Ammo": {
+            "Description": "The ammo that James need to escape",
+            "Damage": 0,
+            "Uses": 1}
+    },
     "James Dave": {
-        "Knife": "25 Damage per hit weapon",
-        "Flaregun": "gun that holds flare ammo to signal for help",
-        "Metal Shard": "shield used to protect against attacks"},
+        "Knife": {
+            "Description": "Small Weapon",
+            "Damage": 25,
+            "Uses": 15},
+        "Flaregun": {
+            "Description": "Uses flare ammo to signal for help",
+            "Damage": 125,
+            "Uses": "Dependent on ammo"},
+        "Metal Shard": {
+            "Description": "Shield used to protect against attacks",
+            "Damage": -50,
+            "Uses": 10},
+    },
     "Jungle Inventory": {
-        "Sword": "weaopn that can be found by James and does 75 Damage",
-        "Adrenaline Shot": "consumable that increases damage by 50"}
+        "Sword": {
+            "Description": "Large Weapon that can be found",
+            "Damage": 75,
+            "Uses": 20},
+        "Adrenaline Shot": {
+            "Description": "Consumable that can be found and increases damage",
+            "Damage": +50,
+            "Uses": 2},
+    }
 }
 
+# Create multiple loops that print statements from the inventory dictionary
 print("\n")
 for inventory, elements in inventories.items():
     message = f"{inventory} has a "
@@ -44,15 +74,18 @@ for inventory, elements in inventories.items():
         message += f"{k}, "
     print(message[:-2] + ".")
     for k, v in elements.items():
-        print(f"{k} is a {v}")
+        print(f"*{k}")
+        print(f"    Description: {v['Description']}")
+        print(f"    Damage: {v['Damage']}")
+        print(f"    Uses: {v['Uses']}")
     print()
 
 
 # Creates a dictionary that stores info about the items in each inventory.
 locations = {
-    "Commerical Plane": "Starting area of the game",
-    "Cave": "Home of Immortal Man",
-    "Jungle": "General area to explore to find items",
+    "The Commerical Plane": "Starting area of the game",
+    "The Cave": "Home of Immortal Man",
+    "The Jungle": "General area to explore to find items",
 }
 
 # A for loop that access's the locations dictionary and prints a description.
