86bf2afe69951792c43d9a93a85734813fb81640
Aarshs <54901482+Aarshs@users.noreply.github.com>
2020-10-08 23:16:01 -0600

    added header to main game file

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+# Course: CS 30
+# Period: 1
+# Date created: 2020-10-08
+# Date last modified: 2020-10-08
+# Name: Aarsh Shah
+# Description: Imports modules/ functions from different game files 
+# and prints them out.
+
+
 from main import large_map, cave_map
 from info import character1, inventory1, location1
 from menu import inventory2, move, start, quit
